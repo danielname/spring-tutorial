72f01717267785e1b705b28ac21675619e8db0f8
Daniel Name <daniel.v.name@gmail.com>
2022-11-19 22:34:08 -0600

    creating student object

--- src/main/java/com/example/practice_example/PracticeExampleApplication.java
+++ src/main/java/com/example/practice_example/PracticeExampleApplication.java
@@ -1,10 +1,12 @@
 package com.example.practice_example;
 
+import com.example.practice_example.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,7 +18,12 @@ public class PracticeExampleApplication {
     }
 
     @GetMapping
-    public List<String> hello(){
-        return List.of("test", "successful");
+    public List<Student> hello(){
+        return List.of(
+                new Student(1L,
+                        "Miriam",
+                        "miriam.jamal@gmail.com",
+                        LocalDate.of())
+        );
     }
 }
