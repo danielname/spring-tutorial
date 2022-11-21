144f51a13a67010c13553c0ad421a3463ea007b1
Daniel Name <daniel.v.name@gmail.com>
2022-11-21 09:08:30 -0600

    created student controller, changed mapping

--- src/main/java/com/example/practice_example/PracticeExampleApplication.java
+++ src/main/java/com/example/practice_example/PracticeExampleApplication.java
@@ -11,22 +11,11 @@ import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class PracticeExampleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PracticeExampleApplication.class, args);
     }
 
-    @GetMapping
-    public List<Student> hello(){
-        return List.of(
-                new Student(1L,
-                        "Miriam",
-                        "miriam.jamal@gmail.com",
-                        LocalDate.of(2000, Month.JANUARY,5),
-                        21
-                )
-        );
-    }
+
 }
